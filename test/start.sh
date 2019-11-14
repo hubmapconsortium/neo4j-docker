0712d6a4d79e74da03022ae2c7a87b1efa6e0d2e
yuanzhou <yuanzhou19@gmail.com>
2019-11-14 13:39:06 -0500

    start.sh update

--- test/start.sh
+++ test/start.sh
@@ -4,7 +4,6 @@
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
 
 # Set the password for native user `neo4j` (must be performed before starting up the database for the first time)
-rm -f /usr/src/app/neo4j/data/dbms/auth
 /usr/src/app/neo4j/bin/neo4j-admin set-initial-password 1234
 
 # Use `console` instead of `start` to keep the terminal window stay open
