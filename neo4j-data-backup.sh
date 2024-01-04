6f712f8ea54f73738c4dea3249d5e6aec7eade1e
yuanzhou <yuanzhou19@gmail.com>
2024-01-04 14:59:14 -0500

    Update neo4j-data-backup.sh

--- neo4j-data-backup.sh
+++ neo4j-data-backup.sh
@@ -5,7 +5,7 @@ echo "==================== Neo4j data backup ===================="
 
 # This function sets DIR to the directory in which this script itself is found.
 # Thank you https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself                                                                      
-function get_dir_of_this_script () {
+function get_dir_of_this_script() {
     SCRIPT_SOURCE="${BASH_SOURCE[0]}"
     while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SCRIPT_SOURCE until the file is no longer a symlink
         DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" >/dev/null 2>&1 && pwd )"
@@ -20,12 +20,12 @@ if [[ "$1" != "localhost" && "$1" != "dev" && "$1" != "test" && "$1" != "stage" 
     echo "Unknown environment '$1', specify 'localhost', 'dev', 'test', 'stage', or 'prod'"
 else
     if [ $# -eq 1 ]; then
-        echo "No backup dir supplied as the second argument"
+        echo "No backup directory specified as the second argument"
         exit 1
     fi
 
     if [ ! -d "$2" ]; then
-        echo "Backup directory $2 does not exists"
+        echo "Backup directory $2 does not exist"
         exit 1
     fi
     
