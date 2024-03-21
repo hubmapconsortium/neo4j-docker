b1106bdc9391782e8ea83ecf38b1d9b2736894bd
yuanzhou <yuanzhou19@gmail.com>
2024-03-20 22:27:33 -0400

    Update neo4j-docker.sh

--- neo4j-docker.sh
+++ neo4j-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ "$1" != "localhost" && "$1" != "dev" && "$1" != "test" && "$1" != "stage" && "$1" != "prod" ]]; then
-    echo "Unknown environment '$1', specify 'localhost', 'dev', 'test', 'stage', or 'prod'"
+if [[ "$1" != "localhost" && "$1" != "dev" && "$1" != "test" && "$1" != "prod" ]]; then
+    echo "Unknown environment '$1', specify 'localhost', 'dev', 'test', or 'prod'"
 else
     if [[ "$2" != "start" && "$2" != "stop" ]]; then
         echo "Unknown command '$2', specify 'start' or 'stop' as the second argument"
