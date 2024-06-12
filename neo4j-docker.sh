28368deea8ca99045b07e5ce1c9f165d686e8ecd
yuanzhou <yuanzhou19@gmail.com>
2024-06-11 21:28:00 -0400

    Remove compatibility mode

--- neo4j-docker.sh
+++ neo4j-docker.sh
@@ -7,9 +7,7 @@ else
         echo "Unknown command '$2', specify 'start' or 'stop' as the second argument"
     else
         if [ "$2" = "start" ]; then
-            # The `--compatibility` flag will attempt to convert deploy keys (memory limit in our case)
-            # in docker compose v3 to their non-Swarm equivalent
-            docker compose -p hubmap-neo4j-docker-$1 -f docker-compose.$1.yml --compatibility up -d
+            docker compose -p hubmap-neo4j-docker-$1 -f docker-compose.$1.yml up -d
         elif [ "$2" = "stop" ]; then
             docker compose -p hubmap-neo4j-docker-$1 -f docker-compose.$1.yml stop
         fi
